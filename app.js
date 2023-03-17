6c6359c39732d842211ed77cfa468cef87e4bae4
NumaanisCoder <nomannaeem985@gmail.com>
2023-03-17 21:32:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000 ;
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("Hello World");
+})
+
+app.listen(port , ()=>{
+    console.log(`Server is On ${port}`);
+})
