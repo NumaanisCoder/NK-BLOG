57ec20c89c35e04fa27efce4eddf02c2a66da14a
NumaanisCoder <nomannaeem985@gmail.com>
2023-04-04 13:41:08 +0530

    yeppp

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const cors = require('cors')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'https://nkblogs.ml',
     credentials: true
   }));
 
