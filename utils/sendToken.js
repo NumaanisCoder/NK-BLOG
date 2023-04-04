0430ab8fd2b9f1d702732fe2aa78cd77eefefe37
NumaanisCoder <nomannaeem985@gmail.com>
2023-04-04 15:42:12 +0530

    ok

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -6,7 +6,8 @@ module.exports.sendToken = async (user, res) => {
         expiresIn: '7d'
     })
     res.cookie('token', token, {
-        expires: new Date(Date.now() + 5 * 5 * 60 * 60 * 1000)
+        expires: new Date(Date.now() + 5 * 5 * 60 * 60 * 1000),
+        secure: true
     })
     
 }
