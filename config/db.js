eddd27134f9326c201ca08fbd79a4a689799c287
NumaanisCoder <nomannaeem985@gmail.com>
2023-04-04 12:04:16 +0530

    suiiii

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports.DBConnection = () => {
 
-    mongoose.connect('mongodb://localhost:27017/NKBLOG-APPLICATION')
+    mongoose.connect('mongodb+srv://admin:00921120Nn@cluster0.lrboy7t.mongodb.net/NK-BLOG?retryWrites=true&w=majority')
     .then(()=>{
         console.log("DB CONNECTED");
     })
@@ -10,3 +10,6 @@ module.exports.DBConnection = () => {
         console.log(`PROBLEM OCCUR AT DB ${err}`);
     })
 }
+
+
+// mongodb+srv://numaan:21032002@cluster0.h0rjrno.mongodb.net/testfarms?retryWrites=true&w=majority
