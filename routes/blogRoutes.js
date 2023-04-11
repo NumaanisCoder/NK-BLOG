f245ac790f795b293a347b5406b57b076b53864b
NumaanisCoder <nomannaeem985@gmail.com>
2023-04-11 16:51:28 +0530

    suii

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -6,7 +6,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 const router = express.Router();
 
-router.route('/createblog/:token').post(isAuthenticated,upload.single("file"),createBlog);
+router.route('/createblog/:token').post(createBlog);
 router.route('/getAllBlogs').get(isAuthenticated,getAllBlogs);
 router.route('/userblogs/:token').get(getUserBlogs);
 router.route('/userblogs/delete/:id').delete(deleteUserBlog);
