8e0a89f09bc0d5a75df6608ed83d30c06c582b6f
NumaanisCoder <nomannaeem985@gmail.com>
2023-04-15 15:23:11 +0530

    updatee

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlSupported = ['https://nkblogs.ml', 'http://localhost:3001'];
-  
+   
 app.use(cors({
     origin: urlSupported,
     credentials: true
